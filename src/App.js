e7d5feb5e57ba39453dc487803bcfbafccd56e0c
Mira Makadia <patelmirak@gmail.com>
2022-03-30 20:04:09 -0400

    testing using routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
+import { Route } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import"./styles.css";
+import About from "./components/About";
+import Work from "./components/Work";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import projectsArr from "./data";
 import { useState, useEffect } from "react";
 
 function App() {
 
-  /* const [data, setData] = useState(null);
 
-  console.log(projectsArr)
 
-  useEffect(() => {setData(projectsArr)}, []);
-  console.log('data',data)*/
-  return ( 
-    
-  <div>
-    <Main />
-    <Footer />
-  </div>
+  return (
+
+    <div>
+      <Main />
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,28 @@
 import "./Main.css"
+import { Route, Switch, Redirect } from "react-router-dom"
+import { Button, Modal } from "react-bootstrap";
+import { useState} from "react";
+import { Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 
 function Main(props) {
+
+    
+
     return (
-        <div className="intro">
-            <div className="introL">left
-                <div className="introLBox">
-                    <h2 className="introSentence">Hey there, My name is </h2>
-                    <h1 className="introName">Mira Makadia</h1>
-                    <div className="titles">
-                        <div className="titlesBox">
-                        <div className="item">Software Developer</div>
-                        <div className="item">MERN Stack Developer</div>
-                        <div className="item">React Developer</div>
-                        </div>
-                    </div>
-                    <div className="buttonsArea">
-                    <div>About me</div>
-                <div>Work</div>
-                <div>Resume</div>
-                <div>Contact</div>
+        <div>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+            </Switch>
 
-                    </div>
-                </div>
-            </div>
-            <div className="introR">right
-            <div className="introRBox">
-               
-
-            </div>
-            </div>
         </div>
-    )
+    ) 
 }
 
 export default Main;
