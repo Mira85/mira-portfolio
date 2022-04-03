0e5708f210280d55b5fa35c9b83fc54748d1c501
Mira Makadia <patelmirak@gmail.com>
2022-04-03 15:19:13 -0400

    adds style to modals

--- src/components/Main.js
+++ src/components/Main.js
@@ -81,164 +81,186 @@ export function Main(props) {
 
     return (
 
-        <div className="intro"  /*style={{ backgroundImage: "url(/images/background-print.png)" }}*/ >
+        <div className="intro" style={{ backgroundImage: "url(/images/background-print.png)" }} >
 
-            <div className="introL">left
-                <div className="introLBox">
-                    <h2 className="introSentence">Hey there, My name is </h2>
-                    <h1 className="introName">Mira Makadia</h1>
-                    <div className="titles">
-                        <div className="titlesBox">
-                            <div className="item">Software Developer</div>
-                            <div className="item">MERN Stack Developer</div>
-                            <div className="item">React Developer</div>
+            <div className="introWrapper">
+                <div className="introL">left
+                    <div className="introLBox">
+                        <h2 className="introSentence">Hey there, My name is </h2>
+                        <h1 className="introName">Mira Makadia</h1>
+                        <div className="titles">
+                            <div className="titlesBox">
+                                <div className="item">Software Developer</div>
+                                <div className="item">MERN Stack Developer</div>
+                                <div className="item">React Developer</div>
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
-            </div>
-            <div className="introR">right
-                <div className="introRBox">
-                    <div className="buttonsArea">
-                        <div className="topButtons">
-                            <Button className="button" style={{ height: "150px", width: "150px" }} onClick={() => handleShow('about')}>About</Button>
-                            <Button className="button" style={{ height: "150px", width: "150px" }} onClick={() => handleShow('experience')}>Experience</Button>
-                        </div>
-                        <div className="bottomButtons" >
-                            <Button className="button" style={{ height: "150px", width: "150px" }} onClick={() => handleShow('resume')}>Resume</Button>
+                <div className="introR">right
+                    <div className="introRBox">
+                        <div className="buttonsArea">
+                            <div className="topButtons">
+                                <Button className="button" style={{ height: "150px", width: "150px" }} onClick={() => handleShow('about')}>About</Button>
+                                <Button className="button" style={{ height: "150px", width: "150px" }} onClick={() => handleShow('experience')}>Experience</Button>
+                            </div>
+                            <div className="bottomButtons" >
+                                <Button className="button" style={{ height: "150px", width: "150px" }} onClick={() => handleShow('resume')}>Resume</Button>
 
-                            <Button className="button" style={{ height: "150px", width: "150px" }} onClick={() => handleShow('contact')}>Contact</Button>
+                                <Button className="button" style={{ height: "150px", width: "150px" }} onClick={() => handleShow('contact')}>Contact</Button>
 
+                            </div>
                         </div>
+
                     </div>
-
                 </div>
-            </div>
-            <Modal show={show.about} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div className="aboutWrapper">
+                <Modal ClassName="your-custom-class" show={show.about} onHide={handleClose}>
 
-                        <Card style={{ maxWidth: "50rem" }}>
-                            <Row className='no-gutters'>
-                                <Col md={5} lg={5}>
-                                    <Card.Img variant="top" src="images/profilepic.png" style={{
-                                        height: "100%",
-                                        width: "100%",
-                                    }} className="image" />
-                                </Col>
-                                <Col>
-                                    <Card.Body>
-                                        <Card.Title className="cardTitle"></Card.Title>
-                                        <Card.Text>
-                                            <p>Hi, my name is Mira. I am a full stack software engineer with a background in the Pharmaceutical Industry. I recently graduated from the Software Engineering Immersive Flex program at General Assembly, where I developed skills in Javascript, React, Python, Django and many other technical skills. During the course I built four projects over the course of 6 months. </p>
-                                            <hr />
-                                            <h5>My skills</h5>
+                    <Modal.Header className="modalHeader " closeButton closeVariant="white">
+                        <Modal.Title>About Me</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="modalBody">
+                        <div className="aboutWrapper">
 
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Col>
-                            </Row>
-                        </Card>
-                    </div>
-                </Modal.Body>
+                            <Card style={{ maxWidth: "90rem", maxHeight: "90rem" }}>
+                                <Row className='no-gutters'>
+                                    <Col md={5} lg={5}>
+                                        <Card.Img variant="top" src="images/profilepic.png" style={{
+                                            height: "100%",
+                                            width: "100%",
+                                        }} className="image" />
+                                    </Col>
+                                    <Col>
+                                        <Card.Body>
+                                            <Card.Title className="cardTitle"></Card.Title>
+                                            <Card.Text>
+                                                <p>Hi, my name is Mira. I am a full stack software engineer with a background in the Pharmaceutical Industry. I recently graduated from the Software Engineering Immersive Flex program at General Assembly, where I developed skills in Javascript, React, Python, Django and many other technical skills. During the course I built four projects over the course of 6 months. </p>
+                                                <hr />
+                                                <h5>My skills:</h5>
+                                                <p>
+                                                    <button>HTML</button>
+                                                    <button>CSS</button>
+                                                    <button>JAVASCRIPT</button>
+                                                    <button>JQUERY</button>
+                                                    <button>PYTHON</button>
+                                                    <button>DJANGO</button>
+                                                    <button>REACT</button>
+                                                    <button>NODE.JS</button>
+                                                    <button>EXPRESS</button>
+                                                    <button>MONGODB</button>
+                                                    <button>POSTGRES</button>
+                                                    <button>BOOTSTRAP</button>
+                                                    <button>MATERIALIZE</button>
+                                                    <button>NETLIFY</button>
+                                                    <button>HEROKU</button>
 
-
-            </Modal>
-
-            <Modal show={show.experience} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-
-                    <div className="projectArea">
-                        {projectsArr.map((object, arrayIdx) => {
-                            return (
-                                <Card key={arrayIdx} style={{ width: "20rem" }} >
-                                    <Card.Img variant="top" src={object.image} style={{
-                                        height: "15.5rem",
-                                        width: "20rem",
-                                    }} className="image" />
-                                    <Card.Body>
-                                        <Card.Title className="cardTitle">{object.name}</Card.Title>
-                                        <Card.Text>
-                                            <div>Description: {object.description}</div>
-                                            <div>Technologies: {object.technologies}</div>
-                                            <Button variant="secondary">
-                                                <a href={object.github} target="_blank">Check Code</a>
-
-                                            </Button>
-                                            <Button variant="secondary">
-                                                <a href={object.viewProject} target="_blank">View Project</a>
-
-                                            </Button>
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-
-                            );
-
-                        })};
-                    </div>
-                </Modal.Body>
+                                                </p>
 
 
-            </Modal>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Col>
+                                </Row>
+                            </Card>
+                        </div>
+                    </Modal.Body>
 
-            <Modal show={show.resume} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Document file={"resume.pdf"} onLoadError={console.error} onLoadSuccess={onDocumentLoadSuccess}>
-                        <Page pageNumber={pageNumber} />
+
+                </Modal>
+
+                <Modal show={show.experience} onHide={handleClose}>
+                    <Modal.Header className="modalHeader " closeButton closeVariant="white">
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="modalBody">
+
+                        <div className="projectArea">
+                            {projectsArr.map((object, arrayIdx) => {
+                                return (
+                                    <Card className="projectCard" key={arrayIdx} style={{ width: "25rem" }} >
+                                        <Card.Img variant="top" src={object.image} style={{
+                                            height: "15.5rem",
+                                            width: "25rem",
+                                        }} className="image" />
+                                        <Card.Body>
+                                            <Card.Title className="cardTitle">{object.name}</Card.Title>
+                                            <Card.Text>
+                                                <div>Description: {object.description}</div>
+                                                <div>Technologies: {object.technologies}</div>
+                                                <Button variant="secondary">
+                                                    <a href={object.github} target="_blank">Check Code</a>
+
+                                                </Button>
+                                                <Button variant="secondary">
+                                                    <a href={object.viewProject} target="_blank">View Project</a>
+
+                                                </Button>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+
+                                );
+
+                            })};
+                        </div>
+                    </Modal.Body>
+
+
+                </Modal>
+
+                <Modal show={show.resume} onHide={handleClose}>
+                    <Modal.Header className="modalHeader " closeButton closeVariant="white">
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="modalBody">
+                        <Document className="resume" file={"resume.pdf"} onLoadError={console.error} onLoadSuccess={onDocumentLoadSuccess}>
+                            <Page pageNumber={pageNumber} />
                         </Document>
-                </Modal.Body>
+                    </Modal.Body>
 
-            </Modal>
-
-
-            <Modal show={show.contact} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form ref={form} onSubmit={sendEmail}>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Full Name</Form.Label>
-                            <Form.Control type="text" name="name" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" name="email" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Subject</Form.Label>
-                            <Form.Control type="text" name="subject" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Message</Form.Label>
-                            <Form.Control as="textarea" rows={5} name="message" />
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-
-                    </Form>
+                </Modal>
 
 
+                <Modal show={show.contact} onHide={handleClose}>
+                    <Modal.Header className="modalHeader " closeButton closeVariant="white">
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="modalBody contactBody">
+                        <Form ref={form} onSubmit={sendEmail} className="contactForm">
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Full Name</Form.Label>
+                                <Form.Control type="text" name="name" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" name="email" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Subject</Form.Label>
+                                <Form.Control type="text" name="subject" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Message</Form.Label>
+                                <Form.Control as="textarea" rows={5} name="message" />
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+
+                        </Form>
 
 
-                    <a href="https://github.com/Mira85" target="_blank"><i class="bi bi-github"> </i></a>
-                    <a href="https://www.linkedin.com/in/mira-makadia/" target="_blank">  <i class="bi bi-linkedin"></i></a>
-                </Modal.Body>
 
-            </Modal>
+                        <div className="contactBtns">
+                            <a href="https://github.com/Mira85" target="_blank"><i class="bi bi-github"> </i></a>
+                            <a href="https://www.linkedin.com/in/mira-makadia/" target="_blank">  <i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </Modal.Body>
 
+                </Modal>
+            </div>
         </div>
     )
 }
