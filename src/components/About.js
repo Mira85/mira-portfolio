fcd4c47f4224699e508ba06a5bc23edba1dc0189
Mira Makadia <patelmirak@gmail.com>
2022-04-04 10:13:27 -0400

    adds untracked files

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import { Modal, Button } from "react-bootstrap";
+
+function About (props) {
+    console.log('aboutprops',props)
+
+    
+    return (<>
+   
+    about
+    
+    </>)
+}
+
+export default About;
